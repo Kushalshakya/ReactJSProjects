a8db15373ab4236b709d325ef4b98aa6f82c89c8
Kushal Shakya <83382379+Kushalshakya@users.noreply.github.com>
2024-04-27 16:57:33 +0545

    Initial commit

---
+++ rockpaperscissors/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './Components/Layout'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <Layout />
+    </>
+  )
+}
